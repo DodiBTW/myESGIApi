377930f4342ee18ab91395ebc3e04847d790bf2e
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2024-11-20 13:03:30 +0100

    Added databasehelper class

---
+++ Data/DatabaseHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace myESGIApi.Data
+{
+    public class DatabaseHelper
+    {
+        private static readonly string? ConnectionString;
+
+        static DatabaseHelper()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+            
+            ConnectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
