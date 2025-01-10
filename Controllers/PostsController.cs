3c1650cbef9d64ac75787b9587ee74f01ab9d519
Ahmad Daoud <115790541+DodiBTW@users.noreply.github.com>
2025-01-11 00:41:52 +0100

    Added favorite Methods and endpoints
    Post method endpoints for favoriting and unfavoriting posts, as well as a get method for retrieving users' favorite posts

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -5,6 +5,7 @@ using MyESGIApi.Models;
 using Utils;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
+using static Utils.Models;
 namespace MyESGIApi.Controllers
 {
     [ApiController]
@@ -12,28 +13,31 @@ namespace MyESGIApi.Controllers
     public class PostsController : ControllerBase 
     {
         [HttpGet(Name = "GetPosts")]
-        public IEnumerable<Post> Get()
+        public IEnumerable<Models.Post> Get()
         {
             return PostsHelper.GetPosts();
         }
         [HttpGet("{id}")]
-        public Post? GetById(int id)
+        public Models.Post? GetById(int id)
         {
             return PostsHelper.GetPostById(id);
         }
         [HttpGet("author/{authorId}")]
-        public IEnumerable<Post> GetByAuthor(int authorId)
+        public IEnumerable<Models.Post> GetByAuthor(int authorId)
         {
             return PostsHelper.GetPostsByAuthorId(authorId);
         }
         [Authorize]
         [HttpPost(Name = "CreatePost")]
-        public IActionResult CreatePost(Post post)
+        public IActionResult CreatePost(Models.Post post)
         {
             var userEmail = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             if (userEmail == null)
                 return Unauthorized("User not logged in");
-            post.AuthorId = UserHelper.GetUserByEmail(userEmail).Id;
+            var user = UserHelper.GetUserByEmail(userEmail);
+            if (user == null)
+                return new NotFoundObjectResult("User not found");
+            post.AuthorId = user.Id;
             var fsPost = post.ConvertToFsharpPost();
             if (!Utils.FormatChecker.CheckPostValid(fsPost))
             {
@@ -42,6 +46,54 @@ namespace MyESGIApi.Controllers
             PostsHelper.CreatePost(post);
             return new OkResult();
         }
-
+        [Authorize]
+        [HttpGet("GetFavorites")]
+        public IActionResult GetUserFavorites()
+        {
+            var email = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (email == null) return Unauthorized("User not logged in");
+            var user = UserHelper.GetUserByEmail(email);
+            return Ok(new { favorites = PostsHelper.GetFavoritePosts(user) });
+        }
+        [Authorize]
+        [HttpPost("FavoritePost")]
+        public IActionResult FavoritePost(int postId)
+        {
+            var userEmail = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userEmail == null)
+                return Unauthorized("User not logged in");
+            var user = UserHelper.GetUserByEmail(userEmail);
+            if (user == null)
+                return new NotFoundObjectResult("User not found");
+            if (!PostsHelper.FavoritePost(postId, user.Id))
+                return new BadRequestObjectResult("Post not found");
+            return new OkResult();
+        }
+        [Authorize]
+        [HttpPost("UnfavoritePost")]
+        public IActionResult UnfavoritePost(int postId)
+        {
+            var userEmail = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userEmail == null)
+                return Unauthorized("User not logged in");
+            var user = UserHelper.GetUserByEmail(userEmail);
+            if (user == null)
+                return new NotFoundObjectResult("User not found");
+            if (!PostsHelper.UnfavoritePost(postId, user.Id))
+                return new BadRequestObjectResult("Post not found");
+            return new OkResult();
+        }
+        [Authorize]
+        [HttpPost("CheckIfFavorite")]
+        public IActionResult CheckIfFavorite(int postId)
+        {
+            var userEmail = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userEmail == null)
+                return Unauthorized("User not logged in");
+            var user = UserHelper.GetUserByEmail(userEmail);
+            if (user == null)
+                return new NotFoundObjectResult("User not found");
+            return Ok(new { isFavorite = PostsHelper.CheckIfFavoritePost(postId, user.Id) });
+        }
     }
 }
